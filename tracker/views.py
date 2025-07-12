ddfd11f20de01e2bf7b253e4976df7f349b7efd2
Bipin Paudel <paudelbipin19@gmail.com>
2025-07-12 12:05:13 +0545

    model aded

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        description = request.POST.get('description')
+        amount = request.POST.get('amount')
+        print(description)
+        return redirect('/')
+
     return render(request, 'index.html')
