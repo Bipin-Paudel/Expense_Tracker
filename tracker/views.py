5cf4219b17e5d1f83356cbb88f4d20fc2bfb7888
Bipin Paudel <paudelbipin19@gmail.com>
2025-07-12 13:48:02 +0545

    connected form and database

--- tracker/views.py
+++ tracker/views.py
@@ -1,11 +1,33 @@
 from django.shortcuts import redirect, render
+from .models import CurrentBalance, TrackingHistory
 
-# Create your views here.
+
 def index(request):
     if request.method == 'POST':
         description = request.POST.get('description')
-        amount = request.POST.get('amount')
-        print(description)
-        return redirect('/')
+        amount = float(request.POST.get('amount'))
+        
+        current_balance, _ = CurrentBalance.objects.get_or_create(id=1)
+        expense_type ='CREDIT'
 
+        if float(amount) < 0:
+            current_balance.current_balance += amount
+            expense_type = 'DEBIT'
+
+        tracking_history = TrackingHistory.objects.create(
+            current_balance = current_balance ,
+            amount = amount,
+            expense_type= expense_type,
+            description = description)
+        
+        current_balance.current_balance += amount
+
+        current_balance.save()
+        return redirect('/')
     return render(request, 'index.html')
+
+
+
+
+
+
